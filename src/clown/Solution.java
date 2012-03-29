4da872b88cb552c61c3f9b3373eb9de095b0c0e4
Simon Andersson <simon.andersson@keybroker.com>
2012-03-29 11:51:40 +0200

    Whitespace

--- src/clown/Solution.java
+++ src/clown/Solution.java
@@ -3,24 +3,23 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
-    public static void main(String args[]) throws java.lang.IllegalAccessException {
+  public static void main(String args[]) throws java.lang.IllegalAccessException {
+    Field[] fields = Volkswagen.class.getDeclaredFields();
 
-        Field[] fields = Volkswagen.class.getDeclaredFields();
+    Volkswagen vw = new Volkswagen();
 
-        Volkswagen vw = new Volkswagen();
+    for (Field field : fields) {
+      if (field.getName() == "clowns") {
+        field.setAccessible(true);
 
-        for (Field field : fields) {
-          if (field.getName() == "clowns") {
-            field.setAccessible(true);
-
-            Set<Clown> newClowns = new HashSet<Clown>();
-            for (int i = 0; i < 20; i++) {
-                newClowns.add(new Clown());
-            }
-
-            field.set(vw, newClowns);
-          }
+        Set<Clown> newClowns = new HashSet<Clown>();
+        for (int i = 0; i < 20; i++) {
+          newClowns.add(new Clown());
         }
-        vw.done();
+
+        field.set(vw, newClowns);
+      }
     }
+    vw.done();
+  }
 }
